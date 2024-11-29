415df5b61976b2fc8550de73271b3e3b55e0cc91
Valijon Ergashev <freedom219307@gmail.com>
2024-11-29 13:07:37 +0500

    blog
    sadfdsf

---
+++ regions-master/region.py
@@ -0,0 +1,29 @@
+# import json
+
+# x = 10
+# x_json = json.dumps(x)
+
+# ism = "anvar"
+# ism_json = json.dumps(ism)
+
+# sonlar = [12, 45, 23, 67]
+# sonlar_json = json.dumps(sonlar)
+# print(x)
+# print(sonlar_json)
+import json
+
+bemor = {
+  "ism": "Alijon Valiyev",
+  "yosh": 30,
+  "oila": True,
+  "farzandlar": ("Ahmad","Bonu"),
+  "allergiya": None,
+  "dorilar": [
+    {"nomi": "Analgin", "miqdori": 0.5},
+    {"nomi": "Panadol", "miqdori": 1.2}
+  ]
+}
+
+bemor_json = json.dumps(bemor)
+print(bemor_json)
+print(type(bemor_json))
